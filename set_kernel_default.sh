c02d7743243162c542ab994716316a305b131b3b
Tao Dai <tao.dai36@gmail.com>
2018-11-01 23:04:36 -0400

    add kernel.sched_migration_cost_ns in set_kernel_default.sh

--- set_kernel_default.sh
+++ set_kernel_default.sh
@@ -3,3 +3,4 @@ sudo sysctl -w vm.dirty_ratio=20
 sudo sysctl -w vm.swappiness=60
 sudo sysctl -w vm.dirty_expire_centisecs=3000
 sudo sysctl -w vm.dirty_writeback_centisecs=500
+sudo sysctl -w kernel.sched_migration_cost_ns=500000
