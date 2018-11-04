46c1f2ec890720f40ae47986f049840e9dbd5133
Tao Dai <tao.dai36@gmail.com>
2018-11-04 01:03:57 -0500

    fix

--- upload_data.sh
+++ upload_data.sh
@@ -2,14 +2,15 @@
 
 # first argument, train or test
 # second argument, number of client
+# third argument, number of iteration
 
 # need to change 
 
 if [ $1 = "train" ]
 then
-    tar -xvzf /home/tdai1/$1_client_$2_batch_100_result.tar -C /home/tdai1/ottertune/client/controller/upload_output
+    tar -xvzf /home/tdai1/$1_client_$2_batch_$3_result.tar -C /home/tdai1/ottertune/client/controller/upload_output
 else
-    tar -xvzf /home/tdai1/test_client_$2_batch_100_result.tar -C /home/tdai1/ottertune/client/controller/upload_output
+    tar -xvzf /home/tdai1/test_client_$2_batch_$3_result.tar -C /home/tdai1/ottertune/client/controller/upload_output
 fi
 
 cd /home/tdai1/ottertune/client/driver
