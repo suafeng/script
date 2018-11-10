37c3459a10aba86d7f47dfa8a85c76e06174168f
Tao Dai <tao.dai36@gmail.com>
2018-11-10 10:59:31 -0500

    fix

--- reload_tpcc.sh
+++ reload_tpcc.sh
@@ -4,4 +4,4 @@ PGPASSWORD=tdai1 createdb -e tpcc -U tdai1
 
 tmux new -d -s longrun
 tmux send -t longrun "cd /home/tdai1" ENTER
-tmux send -t longrun "gunzip -c ~/s500_dump_tpcc.gz | psql tpcc && exit" ENTER
+tmux send -t longrun "gunzip -c ~/s500_dump_tpcc.gz | PGPASSWORD=tdai1 psql tpcc -U tdai1 && exit" ENTER
