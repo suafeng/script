a5bd9514bd0bcfa76544e85e9139852ef9a43086
Tao Dai <tao.dai36@gmail.com>
2018-11-12 21:51:29 -0500

    fix

--- reload_tpcc.sh
+++ reload_tpcc.sh
@@ -2,6 +2,8 @@
 PGPASSWORD=tdai1 dropdb -e --if-exists tpcc -U tdai1
 PGPASSWORD=tdai1 createdb -e tpcc -U tdai1
 
+sudo fstrim /
+
 tmux new -d -s longrun
 tmux send -t longrun "cd /home/tdai1" ENTER
 tmux send -t longrun "gunzip -c ~/s500_dump_tpcc.gz | PGPASSWORD=tdai1 psql tpcc -U tdai1 && exit" ENTER
